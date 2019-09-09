83d17efb7fc0d5e59ec1caf11b9e301a78f33369
Mikhail Gerasimov <mgerasimov@solit-clouds.ru>
2019-09-09 23:00:11 +0300

    timestamp-microservice

--- server.js
+++ server.js
@@ -2,31 +2,44 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/timestamp/:date_string", (req, res) => {
+  const { date_string : dateString } = req.params;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (!dateString) {
+    res.json({ "unix": null, "utc" : "Invalid Date" });
+    return;
+  }
+
+  const isValidDate = (d) => d instanceof Date && !isNaN(d);
+  const date = new Date(dateString);
+
+  if (isValidDate(date)) {
+    res.json({ "unix": date.getTime(), "utc" : date.toUTCString() });
+    return;
+  }
+
+  const dateWithTimestamp = new Date(Number(dateString));
+  if (isValidDate(dateWithTimestamp)) {
+    res.json({ "unix": dateWithTimestamp.getTime(), "utc" : dateWithTimestamp.toUTCString() });
+    return;
+  }
+
+  res.json({ "unix": null, "utc" : "Invalid Date" });
 });
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
